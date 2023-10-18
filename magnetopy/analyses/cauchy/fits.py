d9dece51f5b8bd2894d2047127316da050f73e29
Philip Bunting <77863580+pcb7445@users.noreply.github.com>
2023-10-17 17:30:46 -0700

    add docstrings

--- magnetopy/analyses/cauchy/fits.py
+++ magnetopy/analyses/cauchy/fits.py
@@ -170,9 +170,30 @@ class CauchyParams:
         if isinstance(self.gamma, float):
             self.gamma = tuple(self.gamma)
 
+    def as_dict(self) -> dict[str, Any]:
+        """Returns a dictionary representation of the object.
+
+        Returns
+        -------
+        dict[str, Any]
+            Keys are: "_class_", "m_s", "h_c", "gamma".
+        """
+        output = {"_class_": self.__class__.__name__}
+        output.update(asdict(self))
+        return output
+
 
 @dataclass
 class CauchyFittingArgs:
+    """_summary_
+
+    Attributes
+    ----------
+    terms : list[CauchyParams] | CauchyParams
+        The parameters for each individual Cauchy term to be used in the fit. A single
+        term will be converted to a list of length one.
+    """
+
     terms: list[CauchyParams]
 
     def __post_init__(self):
@@ -181,6 +202,28 @@ class CauchyFittingArgs:
         )
 
     def build_params(self, x_range: tuple[float, float], y_sat: float) -> Parameters:
+        """Uses the `CauchyParams` given during object creation to build a `Parameters`
+        object compatible with `lmfit`.
+
+        Parameters
+        ----------
+        x_range : tuple[float, float]
+            The minimum and maximum values of the x-axis.
+        y_sat : float
+            In terms of magnetization, this is the saturation magnetization of the
+            original M vs. H data. If the data under consideration is the derivative of
+            magnetization with respect to field or is otherwise data in a form similar
+            to that of the Cauchy PDF, y_sat will be half of the integral of the data
+            over the x_range.
+
+        Returns
+        -------
+        Parameters
+            A `Parameters` object compatible with `lmfit`. There will be three parameters
+            (`m_s`, `h_c`, and `gamma`) for each term in `self.terms` and one parameter
+            (`chi_pd`) to account for a phenomena whose contribution to the magnetization
+            is linear with respect to field (i.e., paramagnetism and diamagnetism).
+        """
         params = Parameters()
         for i, term in enumerate(self.terms):
             if len(term.m_s) == 1:
@@ -230,7 +273,20 @@ class CauchyFittingArgs:
         params.add("chi_pd", value=0)
         return params
 
-    def generate_data(self, x: npt.NDArray) -> npt.NDArray:
+    def generate_pdf_data(self, x: npt.NDArray) -> npt.NDArray:
+        """Generates simulated data representing a sum of Cauchy PDFs using the
+        individual terms given during object creation.
+
+        Parameters
+        ----------
+        x : npt.NDArray
+            The x data, e.g., magnetic field.
+
+        Returns
+        -------
+        npt.NDArray
+            The y data, e.g., the derivative of magnetization with respect to field.
+        """
         params = Parameters()
         for i, term in enumerate(self.terms):
             params.add(name=f"m_s_{i}", value=term.m_s[0])
@@ -239,15 +295,48 @@ class CauchyFittingArgs:
         params.add(name="chi_pd", value=0)
         return cauchy_pdf(x, params)
 
+    def as_dict(self) -> dict[str, Any]:
+        """Returns a dictionary representation of the object.
+
+        Returns
+        -------
+        dict[str, Any]
+            Keys are: "_class_", "terms".
+        """
+        output = {"_class_": self.__class__.__name__}
+        output["terms"] = [term.as_dict() for term in self.terms]
+        return output
+
     @classmethod
     def build_from_num_terms(
-        cls, num_terms: int, x_range: tuple[float, float], y_max: float
+        cls, num_terms: int, x_range: tuple[float, float], y_sat: float
     ) -> CauchyFittingArgs:
+        """Generates input parameters for a Cauchy fit based on the number of terms
+        desired.
+
+        Parameters
+        ----------
+        num_terms : int
+            The number of Cauchy terms to be used in the fit.
+        x_range : tuple[float, float]
+            The minimum and maximum values of the x-axis.
+        y_sat : float
+            In terms of magnetization, this is the saturation magnetization of the
+            original M vs. H data. If the data under consideration is the derivative of
+            magnetization with respect to field or is otherwise data in a form similar
+            to that of the Cauchy PDF, y_sat will be half of the integral of the data
+            over the x_range.
+
+        Returns
+        -------
+        CauchyFittingArgs
+            The input parameters for a Cauchy fit.
+        """
         terms = []
         for _ in range(num_terms):
             terms.append(
                 CauchyParams(
-                    m_s=(y_max / num_terms, 0, y_max),
+                    m_s=(y_sat / num_terms, 0, y_sat),
                     h_c=((x_range[0] + x_range[1]) / 2, x_range[0], x_range[1]),
                     gamma=((x_range[1] - x_range[0]) / 10, 0, x_range[1] - x_range[0]),
                 )
@@ -257,6 +346,25 @@ class CauchyFittingArgs:
 
 @dataclass
 class CauchyTermResults:
+    """The fit results for a single Cauchy term. Note that these terms assume that the
+    data was either M vs. H or dM/dH vs. H.
+
+    Attributes
+    ----------
+    m_s : float
+        The saturation magnetization of the term.
+    m_s_err : float
+        The error in the saturation magnetization of the term.
+    h_c : float
+        The coercive field of the term.
+    h_c_err : float
+        The error in the coercive field of the term.
+    gamma : float
+        The parameter describing the broadness of the term.
+    gamma_err : float
+        The error in the parameter describing the broadness of the term.
+    """
+
     m_s: float
     m_s_err: float
     h_c: float
@@ -267,6 +375,33 @@ class CauchyTermResults:
 
 @dataclass
 class CauchyAnalysisResults:
+    """The results of a fit to a Cauchy analysis (either CDF or PDF).
+
+    Attributes
+    ----------
+    terms : list[CauchyTermResults]
+        The fit results for each individual Cauchy term; each term includes values for
+        `m_s`, `h_c`, `gamma`, and their errors.
+    chi_pd : float
+        The fit value for the term describing a phenomena whose contribution to the
+        magnetization is linear with respect to field (i.e., paramagnetism and
+        diamagnetism).
+    chi_pd_err : float
+        The error of `chi_pd`.
+    chi_squared : float
+        The chi-squared value of the fit.
+    reduced_chi_squared : float
+        The reduced chi-squared value of the fit.
+    m_s_unit : str
+        The units of the `m_s`.
+    h_c_unit : str
+        The units of the `h_c`.
+    gamma_unit : str
+        The units of `gamma`.
+    chi_pd_unit : str
+        The units of `chi_pd`.
+    """
+
     terms: list[CauchyTermResults]
     chi_pd: float
     chi_pd_err: float
@@ -278,17 +413,51 @@ class CauchyAnalysisResults:
     chi_pd_unit: str = "unknown"
 
     def set_units(self, m_s: str, h_c: str, gamma: str, chi_pd: str) -> None:
+        """Sets the units of the fit results.
+
+        Parameters
+        ----------
+        m_s : str
+            The units of the `m_s`.
+        h_c : str
+            The units of the `h_c`.
+        gamma : str
+            The units of `gamma`.
+        chi_pd : str
+            The units of `chi_pd`.
+        """
         self.m_s_unit = m_s
         self.h_c_unit = h_c
         self.gamma_unit = gamma
         self.chi_pd_unit = chi_pd
 
     def as_dict(self) -> dict[str, Any]:
+        """Returns a dictionary representation of the object.
+
+        Returns
+        -------
+        dict[str, Any]
+            Keys are: "_class_", "terms", "chi_pd", "chi_pd_err", "chi_squared",
+            "reduced_chi_squared", "m_s_unit", "h_c_unit", "gamma_unit", "chi_pd_unit".
+        """
         output = asdict(self)
         output["_class_"] = self.__class__.__name__
         return output
 
-    def simulate_data(self, x: npt.NDArray) -> npt.NDArray:
+    def simulate_pdf_data(self, x: npt.NDArray) -> npt.NDArray:
+        """Generates simulated data representing a sum of Cauchy PDFs using the
+        individual terms resulting from the fit.
+
+        Parameters
+        ----------
+        x : npt.NDArray
+            The x data, e.g., magnetic field.
+
+        Returns
+        -------
+        npt.NDArray
+            The y data, e.g., the derivative of magnetization with respect to field.
+        """
         fit_params = Parameters()
         for i, term in enumerate(self.terms):
             fit_params.add(name=f"m_s_{i}", value=term.m_s)
@@ -299,6 +468,38 @@ class CauchyAnalysisResults:
 
 
 class CauchyPDFAnalysis:
+    """An analysis of dM/dH vs. H data using a sum of Cauchy probability density
+    functions (PDFs).
+
+    Equation:
+
+    $$
+    \\frac{dM}{dH}(H, M_s, H_c, \\gamma) = \\chi_{pd} + \\
+    \\sum_1^n \\frac{8M_{s,n}}{\pi} \\frac{\\gamma_n}{16 (H-H_{c,n})^2 + \\
+    \\gamma_n^2 }
+    $$
+
+    Parameters
+    ----------
+    dataset : Magnetometry
+        The Magnetometry object containing the data to be analyzed.
+    parsing_args : CauchyParsingArgs
+        The arguments needed to parse the dataset and obtain the data to be analyzed.
+    fitting_args : CauchyFittingArgs | int
+        The arguments needed to perform the fit. If an `int` is given, the number of
+        terms to be used in the fit. If a `CauchyFittingArgs` object is given, the
+        parameters to be used in the fit.
+
+    Attributes
+    ----------
+    parsing_args : CauchyParsingArgs
+        The arguments needed to parse the dataset and obtain the data to be analyzed.
+    fitting_args : CauchyFittingArgs
+        The arguments needed to perform the fit.
+    results : CauchyAnalysisResults
+        The results of the fit.
+    """
+
     def __init__(
         self,
         dataset: Magnetometry,
@@ -320,15 +521,45 @@ class CauchyPDFAnalysis:
 
         self.results = fit_cauchy_pdf(data["h"], data["target"], params)
 
+    def as_dict(self) -> dict[str, Any]:
+        """Returns a dictionary representation of the object.
+
+        Returns
+        -------
+        dict[str, Any]
+            Keys are:
+        """
+        return {
+            "_class_": self.__class__.__name__,
+            "parsing_args": self.parsing_args,
+            "fitting_args": self.fitting_args,
+            "results": self.results,
+        }
+
 
 def cauchy_pdf(h: FitVal, params: Parameters) -> FitVal:
-    # `params` contains
-    #   [
-    #       m_s_0, h_c_0, gamma_0,
-    #       m_s_1, h_c_1, gamma_1,
-    #       ...
-    #       chi_pd
-    #   ]
+    """Generates data representing a sum of Cauchy probability density functions
+    (PDFs).
+
+    Parameters
+    ----------
+    h : FitVal
+        A FitVal (either a float or a numpy array) representing the x data (e.g.,
+        magnetic field).
+    params : Parameters
+        Params must contain the following parameters as a valid `lmfit.Parameters`
+        object:
+            - `m_s_0`, `h_c_0`, `gamma_0`
+            - `m_s_1`, `h_c_1`, `gamma_1`
+            - ...
+            - `chi_pd`
+
+    Returns
+    -------
+    FitVal
+        The float or numpy array representing the y data (e.g., the derivative of
+        magnetization with respect to field).
+    """
     try:
         m = params["chi_pd"]
     except KeyError:
@@ -344,6 +575,26 @@ def cauchy_pdf(h: FitVal, params: Parameters) -> FitVal:
 def fit_cauchy_pdf(
     x: npt.NDArray, y: npt.NDArray, fitting_args: CauchyFittingArgs | int
 ) -> CauchyAnalysisResults:
+    """Fits given x and y data (e.g., magnetic field and the derivative of
+    magnetization with respect to field) to a sum of Cauchy probability density
+    functions (PDFs).
+
+    Parameters
+    ----------
+    x : npt.NDArray
+        The x data, e.g., magnetic field.
+    y : npt.NDArray
+        The y data, e.g., the derivative of magnetization with respect to field.
+    fitting_args : CauchyFittingArgs | int
+        The arguments needed to perform the fit. If an `int` is given, the number of
+        terms to be used in the fit. If a `CauchyFittingArgs` object is given, the
+        parameters to be used in the fit.
+
+    Returns
+    -------
+    CauchyAnalysisResults
+        The results of the fit.
+    """
     x_range = (np.min(x), np.max(x))
     integral_y = np.cumsum(y * np.gradient(x))
     y_sat = (integral_y.max() - integral_y.min()) / 2
